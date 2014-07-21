c903e621fe4a4098f0280be9c556e6f5c5a0a3bb
ahat91 <austinhatem@Austins-MacBook-Pro-2.local>
2014-07-21 19:24:01 -0400

    fixed infinite loop

--- Assets/Spider.js
+++ Assets/Spider.js
@@ -3,16 +3,6 @@ var moveSpeed : float = .10;
 var num1: int = 0;
 var num2: int = 1;
 
-function Start() 
-{
-
-while (true){
-var position: Vector3 = Vector3(num1, 0, 1);
-Debug.Log(position);
-transform.Translate(position * moveSpeed * Time.deltaTime);
-}
-}
-
 function Update () 
 {
 
@@ -22,7 +12,10 @@ animation.Play("Walk");
 //   Attack();
   // yield WaitForSeconds(2.0);
 
-
+var position: Vector3 = Vector3(num1, 0, 1);
+   Debug.Log(position);
+   transform.Translate(position * moveSpeed * Time.deltaTime);
+   
 }
 
 function Attack()
