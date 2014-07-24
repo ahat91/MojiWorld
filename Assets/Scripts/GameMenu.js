7b30939371298dbf4a3f82fc474e08c424e6f1be
ahat91 <austinhatem@Austins-MacBook-Pro-2.local>
2014-07-24 19:51:12 -0400

    Added 2 menus

---
+++ Assets/Scripts/GameMenu.js
@@ -0,0 +1,23 @@
+﻿var isQuit=false;
+
+function OnMouseEnter(){
+//change text color
+renderer.material.color=Color.gray;
+}
+
+function OnMouseExit(){
+//change text color
+renderer.material.color=Color.white;
+}
+
+function OnMouseUp(){
+//is this quit
+if (isQuit==true) {
+//quit the game
+Application.Quit();
+}
+else {
+//load level
+Application.LoadLevel(1);
+}
+}
---
+++ Assets/Scripts/MojiSelect1.js
@@ -0,0 +1,22 @@
+﻿var isChoice=false;
+
+function OnMouseEnter(){
+//change text color
+renderer.material.color=Color.gray;
+}
+
+function OnMouseExit(){
+//change text color
+renderer.material.color=Color.white;
+}
+
+function OnMouseUp(){
+
+if (isChoice==true) {
+
+PlayerPrefs.SetString("Moji", "Moji1");
+//Load Level One
+Application.LoadLevel(1);
+}
+
+}
