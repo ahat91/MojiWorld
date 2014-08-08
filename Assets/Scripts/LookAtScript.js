1ec4cc999095559baee41ce8f6bbd44e54ed9873
naskew100 <naskew100@aol.com>
2014-08-07 20:12:26 -0400

    Souder with attack

--- Assets/Scripts/LookAtScript.js
+++ Assets/Scripts/LookAtScript.js
@@ -2,13 +2,22 @@
 
 public var target : Transform;
 private var goToPlayer : boolean = true;
-private var Spider : Spider;
+public var Spider : Spider;
+public var Action : String;
 
+Action = "Walk";
+function OnTriggerExit (other : Collider)
+{
+if (other.name == "Player1")
+Action = "Walk";
+}
 function OnTriggerEnter(other : Collider)
 {
    if (other.name == "Player1")
    {
+   Action = "Attack";
    goToPlayer = false;
+   Debug.Log("I am happy you are here");
    }
    if (other.name == "SideWall")
    {
@@ -23,7 +32,7 @@ function Update ()
 {
    if (goToPlayer)
    {
-   Spider = GetComponent("Spider");
+   //Spider = GetComponent("Spider");
    
    Spider.moveSpeed = 7;
    Debug.Log(Spider.moveSpeed);
--- Assets/Scripts/Spider.js
+++ Assets/Scripts/Spider.js
@@ -6,15 +6,24 @@ var alphaX: double = .5;
 var Distance;
 public var moveSpeed : float = 3;
 var turnSpeed : float = 50;
+public var Look : LookAtScript;
 /*
 var randomX = Random.RandomRange(0,1);
 var randomZ = Random.RandomRange(0,1);
 var randomRotation = Random.RandomRange(0,100);
 var randomRot: int;
 */
-
+function Start()
+{
+var pos: Vector3 =  new Vector3 (Random.Range(0, 20),1,Random.Range(0, 20));  // Randomize the spawnPoints to instantiate enemy at next.
+ 
+      transform = pos; 
+}
 function Update () 
 {
+
+Look = GetComponent("LookAtScript");
+
 /*
 randomRotation = Random.RandomRange(0,100);
 randomX = Random.RandomRange(-4,4);
@@ -25,7 +34,7 @@ else
 	randomRot = 0;
 	*/
 	
-animation.Play("Walk");
+animation.Play(Look.Action);
 var position: Vector3 = Vector3(0,0,moveSpeed);
    transform.Translate(position * Time.deltaTime);
  //transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, step);
@@ -43,8 +52,8 @@ var position: Vector3 = Vector3(0,0,moveSpeed);
 function Attack()
 {
 
-num1 = Random.Range(-10.0, 10.0);
-num2 = Random.Range(-10.0, 10.0);
+//num1 = Random.Range(-10.0, 10.0);
+//num2 = Random.Range(-10.0, 10.0);
 
 
 }
